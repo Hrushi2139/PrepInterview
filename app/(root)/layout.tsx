f42dacc956bc0487789cd60965f8b87ffe94dcb5
Sannidhanam Hrushikesh Kumar <sannidhanamhrushikesh@gmail.com>
2025-08-31 23:42:35 +0530

    Till auth pages design completed

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+
+const Rootlayout = ({children}: {children: ReactNode}) => {
+
+  return (
+    <div>
+       {children}
+    </div>
+  )
+}
+
+export default Rootlayout
+
