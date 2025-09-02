c8a7615ad25fc0bc4d99e5ed434ba2ddfd66b213
Sannidhanam Hrushikesh Kumar <sannidhanamhrushikesh@gmail.com>
2025-09-02 18:46:02 +0530

    Home Page designing completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,17 @@
+import  Link  from 'next/link'
+import Image from 'next/image'
 import { ReactNode } from 'react'
 
 const Rootlayout = ({children}: {children: ReactNode}) => {
-
   return (
-    <div>
-       {children}
+    <div className='root-layout'>
+      <nav>
+          <Link href="/" className='flex items-center gap-2' >
+            <Image src="/logo.svg" alt="logo" width={38} height={32} /> 
+            <h2 className='text-primary-100'> PrepInterview </h2>
+          </Link>
+      </nav>
+      {children}
     </div>
   )
 }
